0083395ae9f7e50cc0f209aaf9ed7afc56df04a6
Mika Bibas <mikabibas@gmail.com>
2022-11-26 11:42:06 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import logo from './logo.svg';
-import "./App.css";
+import "./styles/App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Nav from "./components/Nav";
